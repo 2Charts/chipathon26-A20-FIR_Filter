fe60f6337586404f84de43e9a34a49746c305804
2Charts <arvinbadri4@gmail.com>
2026-07-30 19:38:30 +0700

    feat: register file

---
+++ src/fir/regfile.sv
@@ -0,0 +1,29 @@
+module regfile #(
+    parameter   XLEN=16,
+    parameter   ADDR_WIDTH=4,
+    localparam  REG_DEPTH=2**ADDR_WIDTH
+) (
+    input  logic [ADDR_WIDTH-1:0]   raddr_i,
+    output logic [XLEN-1:0]         rdata_o,
+
+    input  logic [ADDR_WIDTH-1:0]   waddr_i,
+    input  logic [XLEN-1:0]         wdata_i,
+    input  logic                    wen_i,
+
+    input  logic                    rst_n,
+    input  logic                    clk
+);
+    logic [XLEN-1:0] memory [0:REG_DEPTH-1];
+
+    always_ff @( posedge clk ) begin
+        if(!rst_n) begin
+            foreach(memory[i]) begin
+                memory[i] <= 0;
+            end
+        end else if(wen_i && waddr_i != 0) begin
+            memory[waddr_i] <= wdata_i;
+        end
+    end
+
+    assign rdata_o = memory[raddr_i];
+endmodule
