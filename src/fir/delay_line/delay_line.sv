7721aef4ec732eea78e7f5e4117eb3622314163c
2Charts <arvinbadri4@gmail.com>
2026-07-31 19:54:45 +0700

    refactor: delay line change port names

--- src/fir/delay_line/delay_line.sv
+++ src/fir/delay_line/delay_line.sv
@@ -3,12 +3,12 @@ module delay_line (
     input  logic arst_n,
     
     // Datapath part
-    input  logic [15:0] data_in,         // Input sample
-    output logic [17:0] pre_adder_out,   // Output from pre-adder
+    input  logic [15:0] sample_i,         // Input sample
+    output logic [17:0] sample_o,         // Output from pre-adder
 
     // Controlpath part
-    input  logic        shift_en,
-    input  logic [3:0]  sel,             // For MUX selector
+    input  logic        shift_en_i,
+    input  logic [3:0]  sel_i,             // For MUX sel_iector
     
     // Controlpath mode part (3-bit configuration)
     // Mode encoding:
@@ -17,7 +17,7 @@ module delay_line (
     // 101 -> Symmetric, Odd
     // 110 -> Anti-symmetric, Even
     // 111 -> Anti-symmetric, Odd
-    input  logic [2:0]  mode_config 
+    input  logic [2:0]  mode_i 
 );
 
     // SIPO processing 16x16, 16x1, 16x15
@@ -25,14 +25,14 @@ module delay_line (
     logic [15:0] sipo_mid;       // Center tap
     logic [15:0] sipo_bot [1:15]; // Taps 15-1
 
-    // Explicit control signals decoding based on 3-bit mode_config
+    // Explicit control signals decoding based on 3-bit mode_i
     logic mode_asym_active;
     logic mode_antisym_active;
     logic mode_odd_active;
 
-    assign mode_asym_active    = (mode_config[2] == 1'b0);
-    assign mode_antisym_active = (mode_config[2] == 1'b1) && (mode_config[1] == 1'b1);
-    assign mode_odd_active     = (mode_config[2] == 1'b1) && (mode_config[0] == 1'b1);
+    assign mode_asym_active    = (mode_i[2] == 1'b0);
+    assign mode_antisym_active = (mode_i[2] == 1'b1) && (mode_i[1] == 1'b1);
+    assign mode_odd_active     = (mode_i[2] == 1'b1) && (mode_i[0] == 1'b1);
 
     // SIPO shifting (Shift Register)
     always_ff @(posedge clk or negedge arst_n) begin
@@ -41,9 +41,9 @@ module delay_line (
             sipo_mid <= 16'd0;
             for (int i=1; i<=15; i++) sipo_bot[i] <= 16'd0;
         end
-        else if (shift_en) begin
+        else if (shift_en_i) begin
             // Shift top (16x16)
-            sipo_top[1] <= data_in;
+            sipo_top[1] <= sample_i;
             for (int i=2; i<=16; i++) sipo_top[i] <= sipo_top[i-1];
             
             // Shift mid (16x1)
@@ -68,21 +68,21 @@ module delay_line (
 
     always_comb begin
         // Top MUX (Taps 31-16)
-        mux_top = sipo_top[16-sel]; 
+        mux_top = sipo_top[16-sel_i]; 
 
-        // Bottom MUX (Taps 15-1 and data_in)
-        if (sel == 4'd15) begin
-            mux_bot = data_in;
+        // Bottom MUX (Taps 15-1 and sample_i)
+        if (sel_i == 4'd15) begin
+            mux_bot = sample_i;
         end else begin
-            mux_bot = sipo_bot[15-sel];
+            mux_bot = sipo_bot[15-sel_i];
         end
         
         // Zeroing logic based on active modes
         if (mode_asym_active == 1'b1) begin
             // 0xx: Asymmetric -> Drop ALL bottom data (A + 0)
             mux_bot_routed = 16'd0;
-        end else if (mode_odd_active == 1'b1 && sel == 4'd15) begin
-            // Odd mode -> Drop ONLY the center tap at sel=15
+        end else if (mode_odd_active == 1'b1 && sel_i == 4'd15) begin
+            // Odd mode -> Drop ONLY the center tap at sel_i=15
             mux_bot_routed = 16'd0;
         end else begin
             // Normal symmetric / antisymmetric routing
@@ -107,6 +107,6 @@ module delay_line (
         end
     end
 
-    assign pre_adder_out = top_ext + bot_ext;
+    assign sample_o = top_ext + bot_ext;
 
 endmodule
--- src/fir/fir_top.sv
+++ src/fir/fir_top.sv
@@ -1,34 +1,45 @@
 module fir_top (
     // AXI-Stream Slave (Input)
-    input   logic [15:0]    s_axis_tdata,
-    input   logic           s_axis_tvalid,
-    output  logic           s_axis_tready,
+    input   logic [15:0]    s_axis_tdata_i,
+    input   logic           s_axis_tvalid_i,
+    output  logic           s_axis_tready_o,
     
     // AXI-Stream Master (Output)
-    output  logic [15:0]    m_axis_tdata,
-    output  logic           m_axis_tvalid,
-    input   logic           m_axis_tready,
+    output  logic [15:0]    m_axis_tdata_o,
+    output  logic           m_axis_tvalid_o,
+    input   logic           m_axis_tready_i,
 
-    // Programming interface
-    input   logic [7:0]     prog_data,
-    input   logic           prog_valid,
+    // Registers interface
+    // 7-bit config register
+    input   logic [6:0]     config_data_i,
+    input   logic           config_wr_en_i,
+    // 16 x 16-bit coefficient registers
+    input   logic [15:0]    coeff_data_i,
+    input   logic [3:0]     coeff_addr_i,
+    input   logic           coeff_wr_en_i,
 
     input   logic           arst_n,
     input   logic           clk
 );
-    /* FIR CONFIGURATION REGISTERS */
-    logic [2:0]     config_delay_line;
+    /* FIR CONFIGURATION REGISTER */
 
+    // 7 bit config Register
+    // MSB [ D D D R R R R ] LSB
+    // D : Delay Line Mux
+    // R : Reserved
+    logic [6:0]     config_register;
 
-    /* FIR PROGRAMMING DATAPATH */
+    logic [2:0]     delay_line_sel;
+    assign delay_line_sel = config_register[6:4];
 
-    // new coefficient data
-    logic           coeff_wr_en;
-    logic [3:0]     coeff_wr_addr;
-    logic [15:0]    coeff_wr_data;
-
-    // integrate programming FSM
-
+    always_ff @(posedge clk or negedge arst_n) begin
+        if (!arst_n) begin
+            config_register <= 0;
+        end
+        else if (config_wr_en_i) begin
+            config_register <= config_data_i;
+        end
+    end
 
     /* FIR DATAPATH */
 
@@ -41,12 +52,34 @@ module fir_top (
         .raddr_i(coeff_rd_addr),
         .rdata_o(coeff_rd_data),
 
-        .waddr_i(coeff_wr_addr),
-        .wdata_i(coeff_wr_data),
+        .waddr_i(coeff_addr_i),
+        .wdata_i(coeff_data_i),
         .wen_i(coeff_wr_en),
 
         .clk(clk)
     );
+
+    // delay_line delay_line (
+    // input  logic clk,
+    // input  logic arst_n,
+    
+    // // Datapath part
+    // input  logic [15:0] sample_i,         // Input sample
+    // output logic [17:0] sample_o,         // Output from pre-adder
+
+    // // Controlpath part
+    // input  logic        shift_en_i,
+    // input  logic [3:0]  sel_i,             // For MUX sel_iector
+    
+    // // Controlpath mode part (3-bit configuration)
+    // // Mode encoding:
+    // // 0xx -> Asymmetric mode
+    // // 100 -> Symmetric, Even
+    // // 101 -> Symmetric, Odd
+    // // 110 -> Anti-symmetric, Even
+    // // 111 -> Anti-symmetric, Odd
+    // input  logic [2:0]  mode_i 
+    // );
 
     // add delay line, MAC
 
